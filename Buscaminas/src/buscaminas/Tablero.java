6887fa041069289ae6812801ead9e6b694aea06b
XSA <XSA@LUR-27>
2016-02-22 17:00:58 +0100

    Sobraba una clase Tablero.java
    Se renombra Tablero00 por Tablero

--- Buscaminas/src/buscaminas/Tablero.java
+++ Buscaminas/src/buscaminas/Tablero.java
@@ -1,5 +1,51 @@
 package buscaminas;
 
-public class Tablero {
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Tablero extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tablero frame = new Tablero();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Tablero() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(9, 7, 0, 0));
+
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 8; j++) {
+				JButton btnNewButton = new JButton("(" + i + "," + j + ")");
+				contentPane.add(btnNewButton);
+				btnNewButton.addActionListener(new ManejadorDePulsarBotón());
+			}
+		}
+	}
 
 }
